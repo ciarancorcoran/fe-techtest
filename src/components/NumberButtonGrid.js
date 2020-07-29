1263e0c0390ef7a1744908ef03fb920cf8b9161f
Ciaran Corcoran <corcoranciaran01@gmail.com>
2020-07-29 15:52:38 +0100

    moving function to component and create util function for calculation

--- src/components/NumberButtonGrid.js
+++ src/components/NumberButtonGrid.js
@@ -1,5 +1,19 @@
-import { handleNumberButtonClick } from '../utils/numberButtonsClick'
+import { checkNumberIsMultiple } from '../utils/numberButtonsClick'
 import './NumberButtonGrid.css';
+
+export const handleNumberButtonClick = (e, max) => {
+  if (e.target.tagName !== 'BUTTON') { return }
+
+  const number = Number(e.target.innerHTML)
+  for (let i = 1; i <= max; i++) {
+    const idToHighlight = document.getElementById(i)
+    if (!idToHighlight.classList.contains('highlight') && checkNumberIsMultiple(i, number)) {
+      idToHighlight.classList.add('highlight')
+    } else if (idToHighlight.classList.contains('highlight') && !checkNumberIsMultiple(i, number)) {
+      idToHighlight.classList.remove('highlight')
+    }
+  }
+}
 
 const NumberButtonGrid = (max) => {
   const buttonGridContainer = document.createElement('main')
--- src/utils/numberButtonsClick.js
+++ src/utils/numberButtonsClick.js
@@ -1,13 +1,1 @@
-export const handleNumberButtonClick = (e, max) => {
-  if (e.target.tagName !== 'BUTTON') { return }
-
-  const number = Number(e.target.innerHTML)
-  for (let i = 1; i <= max; i++) {
-    const idToHighlight = document.getElementById(i)
-    if (!idToHighlight.classList.contains('highlight') && i % number === 0) {
-      idToHighlight.classList.add('highlight')
-    } else if (idToHighlight.classList.contains('highlight') && i % number !== 0) {
-      idToHighlight.classList.remove('highlight')
-    }
-  }
-}
+export const checkNumberIsMultiple = (currentNumber, clickedNumber) => currentNumber % clickedNumber === 0 ? true : false
