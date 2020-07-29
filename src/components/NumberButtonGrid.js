f1f708cd48d713c57c74f9c017aea31c53c3e759
Ciaran Corcoran <corcoranciaran01@gmail.com>
2020-07-29 16:57:02 +0100

    moving function

--- src/components/NumberButtonGrid.js
+++ src/components/NumberButtonGrid.js
@@ -1,32 +1,30 @@
-import { checkNumberIsMultiple } from '../utils/numberButtonsClick'
+import { checkNumberIsMultiple } from '../utils/checkNumberIsMultiple'
 import './NumberButtonGrid.css';
 
-const handleNumberButtonClick = (e, numbersArray) => {
-  if (e.target.tagName !== 'BUTTON') { return }
 
-  const number = Number(e.target.innerHTML)
-
-  // go through the array and highlight each number that is a multiple
-  numbersArray.forEach(currNum => {
-    if (currNum === 0) {
-      return
-    }
-
-    const idToHighlight = document.getElementById(currNum)
-
-    if (checkNumberIsMultiple(currNum, number)) {
-      idToHighlight.classList.add('highlight')
-      return
-    }
-
-    idToHighlight.classList.remove('highlight')
-  })
-}
 
 const NumberButtonGrid = (max) => {
+
+  const handleNumberButtonClick = (e, numbersArray) => {
+    if (e.target.tagName !== 'BUTTON') { return }
+    const number = Number(e.target.innerHTML)
+
+    // go through the array and highlight each number that is a multiple
+    numbersArray.forEach(currNum => {
+      if (currNum === 0) {
+        return
+      }
+      const idToHighlight = document.getElementById(currNum)
+      if (checkNumberIsMultiple(currNum, number)) {
+        idToHighlight.classList.add('highlight')
+        return
+      }
+      idToHighlight.classList.remove('highlight')
+    })
+  }
+
   const buttonGridContainer = document.createElement('main')
   const numbersArray = Array.from({ length: max }, (v, i) => i)
-  console.log('here it is: ', numbersArray)
   buttonGridContainer.addEventListener('click', event => handleNumberButtonClick(event, numbersArray))
 
   for (let i = 1; i <= max; i++) {
